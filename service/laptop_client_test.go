50eb02cf09b41be36372609627c0f2994ae11d3b
hwdhy <1284066462@qq.com>
2022-01-16 15:18:56 +0800

    流式接收  单元测试

--- service/laptop_client_test.go
+++ service/laptop_client_test.go
@@ -14,6 +14,7 @@ import (
 	"grpc_project/sample"
 	"grpc_project/serializer"
 	"grpc_project/service"
+	"io"
 	"net"
 	"testing"
 )
@@ -21,7 +22,7 @@ import (
 func TestClientCreateLaptop(t *testing.T) {
 	t.Parallel()
 
-	laptopServer, serverAddress := startTestLaptopServer(t)
+	laptopServer, serverAddress := startTestLaptopServer(t, service.NewInmemoryLaptopStore())
 	laptopClient := newTestLaptopClient(t, serverAddress)
 
 	laptop := sample.NewLaptop()
@@ -41,8 +42,88 @@ func TestClientCreateLaptop(t *testing.T) {
 	requireSampLaptop(t, laptop, other)
 }
 
-func startTestLaptopServer(t *testing.T) (*service.LaptopServer, string) {
-	laptopServer := service.NewLaptopServer(service.NewInmemoryLaptopStore())
+func TestClientSearchLaptop(t *testing.T) {
+	t.Parallel()
+
+	filter := &pb.Filter{
+		MaxPriceUsd: 2000,
+		MinCpuCores: 4,
+		MinCpuGhz:   2.2,
+		MinRam: &pb.Memory{
+			Value: 4,
+			Unit:  pb.Memory_GIGABYTE,
+		},
+	}
+
+	store := service.NewInmemoryLaptopStore()
+	expectedIds := make(map[string]bool)
+
+	for i := 0; i < 6; i++ {
+		laptop := sample.NewLaptop()
+
+		switch i {
+		case 0:
+			laptop.PriceUsd = 3000
+		case 1:
+			laptop.Cpu.NumberCores = 2
+		case 2:
+			laptop.Cpu.MinGhz = 2.0
+		case 3:
+			laptop.Ram = &pb.Memory{
+				Value: 4096,
+				Unit:  pb.Memory_MEGABYTE,
+			}
+		case 4:
+			laptop.PriceUsd = 1999
+			laptop.Cpu.NumberCores = 4
+			laptop.Cpu.MinGhz = 2.5
+			laptop.Cpu.MaxGhz = 4.5
+			laptop.Ram = &pb.Memory{
+				Value: 16,
+				Unit:  pb.Memory_GIGABYTE,
+			}
+			expectedIds[laptop.Id] = true
+		case 5:
+			laptop.PriceUsd = 2000
+			laptop.Cpu.NumberCores = 4
+			laptop.Cpu.MinGhz = 2.5
+			laptop.Cpu.MaxGhz = 4.5
+			laptop.Ram = &pb.Memory{
+				Value: 16,
+				Unit:  pb.Memory_GIGABYTE,
+			}
+			expectedIds[laptop.Id] = true
+		}
+
+		err := store.Save(laptop)
+		require.NoError(t, err)
+	}
+	_, serverAddress := startTestLaptopServer(t, store)
+	laptopClient := newTestLaptopClient(t, serverAddress)
+
+	req := &pb.SearchLaptopRequest{
+		Filter: filter,
+	}
+	stream, err := laptopClient.SearchLaptop(context.Background(), req)
+	require.NoError(t, err)
+
+	found := 0
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		require.NoError(t, err)
+		require.Contains(t, expectedIds, res.GetLaptop().GetId())
+		found++
+	}
+
+	require.Equal(t, found, len(expectedIds))
+}
+
+// startTestLaptopServer 返回server 并返回端口
+func startTestLaptopServer(t *testing.T, store service.LaptopStore) (*service.LaptopServer, string) {
+	laptopServer := service.NewLaptopServer(store)
 
 	grpcServer := grpc.NewServer()
 	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
@@ -55,6 +136,7 @@ func startTestLaptopServer(t *testing.T) (*service.LaptopServer, string) {
 	return laptopServer, listen.Addr().String()
 }
 
+// newTestLaptopClient 返回client端
 func newTestLaptopClient(t *testing.T, serverAddress string) pb.LaptopServiceClient {
 	conn, err := grpc.Dial(serverAddress, grpc.WithInsecure())
 	require.NoError(t, err)
@@ -62,6 +144,7 @@ func newTestLaptopClient(t *testing.T, serverAddress string) pb.LaptopServiceCli
 	return pb.NewLaptopServiceClient(conn)
 }
 
+// 比较protobuf是否相等
 func requireSampLaptop(t *testing.T, laptop1 *pb.Laptop, laptop2 *pb.Laptop) {
 	json1, err := serializer.ProtobufToJSON(laptop1)
 	require.NoError(t, err)
